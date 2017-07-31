55e2b5878a7f2d7d30c058598d0e0df0fd9987c1
Andrei Cristian Pascu <pascuandreicristian@yahoo.com>
2017-07-31 21:29:51 +0300

    Update login.php

--- login.php
+++ login.php
@@ -42,7 +42,6 @@
   </head>
   <body>
     <h2>Login Page</h2>
-    <a href="register.php">REGISTER<br/><br/></a>
     <form action="checklogin.php" method="POST">
       Enter Username: <input type="text" name="username" required="required" /> <br/>
       Enter Password: <input type="password" name="password" required="required" /> <br/>
