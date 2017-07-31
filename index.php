70b0740f757b63174f4f70c47cc66d243c38391a
Andrei Cristian Pascu <pascuandreicristian@yahoo.com>
2017-07-31 20:49:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
       Print "</div>";
     }
 ?>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
 </script>
 <script>
   $(document).ready(function(){
