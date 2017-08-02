5ffe7142e2e2ce5928c2e476473dd6ed6f3e7e54
Andrei Cristian Pascu <pascuandreicristian@yahoo.com>
2017-08-02 15:33:29 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@
   };
   if($_SERVER['REQUEST_METHOD'] == "GET")
   {
-    $link = mysqli_connect("localhost", "root", "", "esports") or die(mysql_error());
+    $link = mysqli_connect("eu-cdbr-west-01.cleardb.com", "b8f790c79aeba6", "f66a1016", "heroku_5f5cb1110080006") or die(mysql_error());
     $id = $_GET['id'];
     mysqli_query($link, "DELETE FROM news WHERE id='$id'");
     header("location: newsletter.php");
