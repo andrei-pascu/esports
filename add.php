8399ba7f8bd5541537c2e6df52a90a54e30bfb4c
Andrei Cristian Pascu <pascuandreicristian@yahoo.com>
2017-08-02 15:32:08 +0300

    Update add.php

--- add.php
+++ add.php
@@ -1,6 +1,6 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-  $link=mysqli_connect("localhost", "root", "", "esports");
+  $link = mysqli_connect("eu-cdbr-west-01.cleardb.com", "b8f790c79aeba6", "f66a1016", "heroku_5f5cb1110080006");
 	$title = mysqli_real_escape_string($link, $_POST['title']);
 	$date = strftime("%B %d, %Y");
 	$content = mysqli_real_escape_string($link, $_POST['content']);
